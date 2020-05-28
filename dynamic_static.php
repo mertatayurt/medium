29446e9790565a5bd536909f14188b1053a8370b
Mert <mertatayurt@users.noreply.github.com>
2020-05-28 11:19:00 +0300

    dynamic_static.php

---
+++ dynamic_static.php
@@ -0,0 +1,22 @@
+
+<?php
+
+$error = true;
+
+
+$message = null;
+
+
+if ($error) {
+
+
+   $message = 'You have encountered an error';
+
+
+   // Original variable type of null now cast to a string
+
+
+}
+
+
+echo $message; // 'You have encountered an error'
